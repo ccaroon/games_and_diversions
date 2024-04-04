42b19b0127847d992d9a0e7b4f670b5a81b9fc26
Craig N. Caroon <craig.caroon@cengage.com>
2024-04-04 14:53:18 -0400

    -- boids -- * pretty much working now * cleaned up a bit * add some new cli args

--- boids/boids.py
+++ boids/boids.py
@@ -5,17 +5,28 @@ import curses
 from boids.simulation import Simulation
 
 def main(stdscr, args):
-    boids = Simulation(stdscr, args.count)
+    boids = Simulation(
+        stdscr,
+        args.count,
+        iterations=args.iterations,
+        delay=args.delay,
+        marker=args.marker
+    )
     boids.run()
 
-    stdscr.addstr(curses.LINES-1, 0, "|--Any Key to Exit--", curses.A_REVERSE)
+    stdscr.addstr("--Any Key to Exit--", curses.A_REVERSE)
+    stdscr.nodelay(False)
     stdscr.getch()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Boids - Flocking Simulation"
     )
-    parser.add_argument("--count", type=int, default=10, required=False)
+    parser.add_argument("--count", "-c", type=int, default=10, required=False, help="Number of Boids")
+    parser.add_argument("--iterations", "-i", type=int, default=100, required=False, help="Number of Iterations to run")
+    parser.add_argument("--delay", "-d", type=float, default=0.25, required=False, help="Delay between interations")
+    parser.add_argument("--marker", "-m", type=str, default=".", required=False, help="Character to use as Boid marker")
+
     args = parser.parse_args()
 
     curses.wrapper(main, args)
--- boids/boids/simulation.py
+++ boids/boids/simulation.py
@@ -1,23 +1,22 @@
-#!/usr/bin/env python
 import curses
 import math
+import random
 import time
 
 import numpy as np
 from scipy.spatial.distance import squareform, pdist
 from numpy.linalg import norm
 
-
 class Simulation:
-    MIN_DISTANCE = 15.0
+    MIN_DISTANCE = 1.0
     MAX_RULE_VELO = 0.03
-    MAX_VELOCITY = 0.75
+    MAX_VELOCITY = .50
 
     COLOR_BOID = 1
     COLOR_BLACK_GREEN = 2
     COLOR_DEBUG = 3
 
-    def __init__(self, stdscr, count):
+    def __init__(self, stdscr, count, **kwargs):
         curses.init_pair(self.COLOR_BOID,
                          curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(self.COLOR_BLACK_GREEN,
@@ -25,43 +24,50 @@ class Simulation:
         curses.init_pair(self.COLOR_DEBUG,
                          curses.COLOR_RED, curses.COLOR_BLACK)
 
-        self.__screen = stdscr
+        self.__marker = kwargs.get("marker", ".")
+        self.__iteration_count = kwargs.get("iterations", 100)
+        self.__delay = kwargs.get("delay", 0.25)
 
-        self.WIDTH = curses.COLS
-        self.HEIGHT = curses.LINES
+        self.__screen = stdscr
+        self.__screen.nodelay(True)
+
+        self.__width = curses.COLS - 2
+        self.__height = curses.LINES - 1
 
         # position of each boid
         # ...each being a random distance from the center up to 10 units away
         # ...represented a `count` pairs of (x,y) coordinates
-        self.pos = [self.WIDTH / 2.0, self.HEIGHT / 2.0] + 15 * np.random.rand(2 * count).reshape(count, 2)
+        self.__boids = [self.__width / 2.0, self.__height / 2.0] + 5 * np.random.rand(2 * count).reshape(count, 2)
 
         angles = 2 * math.pi * np.random.rand(count)
         self.vel = np.array(list(zip(np.cos(angles), np.sin(angles))))
-        self.count = count
+        self.__count = count
+
+        self.__iteration = 1
 
     def apply_boundary(self):
-        delta_r = 2.0
+        delta_r = 0.0
 
-        for coord in self.pos:
-            if coord[0] > self.WIDTH + delta_r:
+        for coord in self.__boids:
+            if coord[0] > self.__width + delta_r:
                 coord[0] = -delta_r
 
             if coord[0] < -delta_r:
-                coord[0] = self.WIDTH + delta_r
+                coord[0] = self.__width + delta_r
 
-            if coord[1] > self.HEIGHT + delta_r:
+            if coord[1] > self.__height + delta_r:
                 coord[1] = -delta_r
 
             if coord[1] < -delta_r:
-                coord[1] = self.HEIGHT + delta_r
+                coord[1] = self.__height + delta_r
 
     def apply_rules(self):
         # get pair-wise distances
-        dist_matrix = squareform(pdist(self.pos))
+        dist_matrix = squareform(pdist(self.__boids))
 
         # rule #1: separation
         D = dist_matrix < self.MIN_DISTANCE
-        vel = self.pos * D.sum(axis=1).reshape(self.count, 1) - D.dot(self.pos)
+        vel = self.__boids * D.sum(axis=1).reshape(self.__count, 1) - D.dot(self.__boids)
         self.limit(vel, self.MAX_RULE_VELO)
 
         D = dist_matrix < 50.0
@@ -72,7 +78,7 @@ class Simulation:
         vel += vel2
 
         # rule #3 - cohesion
-        vel3 = D.dot(self.pos) - self.pos
+        vel3 = D.dot(self.__boids) - self.__boids
         self.limit(vel3, self.MAX_RULE_VELO)
         vel += vel3
 
@@ -87,48 +93,69 @@ class Simulation:
         if mag > max_val:
             vec[0], vec[1] = vec[0]*max_val/mag, vec[1]*max_val/mag
 
-    def button_press(self, event):
-        if event.button == 1:
-            self.pos = np.concatenate(
-                (self.pos, np.array([[event.xdata, event.ydata]])),
-                axis=0
-            )
-            angles = 2 * math.pi * np.random.rand(1)
-            v = np.array(list(zip(np.sin(angles), np.cos(angles))))
-            self.vel = np.concatenate((self.vel, v), axis=0)
+    def add(self, loc:tuple=None):
+        # Add a new Boid
+        row = loc[0] or (self.__height // 2)
+        col = loc[1] or (self.__width // 2)
+        self.__boids = np.concatenate(
+            (self.__boids, np.array([[col,row]])),
+            axis=0
+        )
+        angles = 2 * math.pi * np.random.rand(1)
+        v = np.array(list(zip(np.sin(angles), np.cos(angles))))
+        self.vel = np.concatenate((self.vel, v), axis=0)
 
-            self.count += 1
-        elif event.button == 3:
-            self.vel += 0.1 * (self.pos - np.array([[event.xdata, event.ydata]]))
+        self.__count += 1
+
+    def scatter(self, loc:tuple=None):
+        row = loc[0] or random.randint(0, self.__height)
+        col = loc[1] or random.randint(0, self.__width)
+        self.vel += 0.1 * (self.__boids - np.array([[col, row]]))
 
     def tick(self):
         self.vel += self.apply_rules()
         self.limit(self.vel, self.MAX_VELOCITY)
-        self.pos += self.vel
+        self.__boids += self.vel
         self.apply_boundary()
 
-        self.pos.reshape(2*self.count)[::2],
-        self.pos.reshape(2*self.count)[1::2]
+        self.__boids.reshape(2*self.__count)[::2],
+        self.__boids.reshape(2*self.__count)[1::2]
+
+        self.__iteration += 1
 
     def display(self):
-        self.__screen.addstr(0, 70, "Craig N. Caroon",curses.color_pair(self.COLOR_DEBUG))
-        # line = 1
-        for boid in self.pos:
+        self.__screen.erase()
+        for boid in self.__boids:
             col = int(boid[0])
             row = int(boid[1])
-            self.__screen.addstr(row, col, ".", curses.color_pair(self.COLOR_BOID))
+            self.__screen.addstr(row, col, self.__marker, curses.color_pair(self.COLOR_BOID))
 
-            # self.__screen.addstr(line, 0, f"{row},{col}", curses.color_pair(self.COLOR_DEBUG))
-            # line += 1
+        self.__screen.addstr(
+            self.__height, 0,
+            f"Boids| {self.__width}x{self.__height} | {self.__count} | {self.__iteration}/{self.__iteration_count} | ",
+            curses.color_pair(self.COLOR_BLACK_GREEN)
+        )
 
         self.__screen.refresh()
 
     def run(self):
-        # self.display()
-        # self.tick()
-        # time.sleep(1)
-        # self.display()
-        for _ in range(100):
+        curses.mousemask(curses.ALL_MOUSE_EVENTS)
+        for _ in range(self.__iteration_count):
             self.display()
             self.tick()
-            time.sleep(0.25)
+
+            input = self.__screen.getch()
+            if input == ord("q"):
+                break
+            elif input == ord("s"):
+                self.scatter()
+            elif input == ord("b"):
+                self.add()
+            elif input == curses.KEY_MOUSE:
+                id,x,y,_,button = curses.getmouse()
+                if button & curses.BUTTON1_CLICKED:
+                    self.add((y,x))
+                if button & curses.BUTTON2_CLICKED:
+                    self.scatter((y,x))
+
+            time.sleep(self.__delay)
