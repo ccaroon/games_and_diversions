12a81d72c02aee681da6d07cb882c583026b1245
Craig N. Caroon <craig.caroon@cengage.com>
2024-04-05 16:04:57 -0400

    -- boids -- * Added --debug arg * typo corrections * Changed default marker to "middle dot" * playing around with boundary conditions (reflect instead of wrap) * playing around with starting seeding of boids

--- boids/boids.py
+++ boids/boids.py
@@ -10,7 +10,8 @@ def main(stdscr, args):
         args.count,
         iterations=args.iterations,
         delay=args.delay,
-        marker=args.marker
+        marker=args.marker,
+        debug=args.debug
     )
     boids.run()
 
@@ -23,10 +24,13 @@ if __name__ == "__main__":
         description="Boids - Flocking Simulation"
     )
     parser.add_argument("--count", "-c", type=int, default=10, required=False, help="Number of Boids")
-    parser.add_argument("--iterations", "-i", type=int, default=100, required=False, help="Number of Iterations to run")
-    parser.add_argument("--delay", "-d", type=float, default=0.25, required=False, help="Delay between interations")
-    parser.add_argument("--marker", "-m", type=str, default=".", required=False, help="Character to use as Boid marker")
+    parser.add_argument("--iterations", "-i", type=int, default=100, required=False, help="Number of iterations to run")
+    parser.add_argument("--delay", "-d", type=float, default=0.10, required=False, help="Delay between iterations")
+    parser.add_argument("--marker", "-m", type=str, default="·", required=False, help="Character to use as Boid marker")
+    parser.add_argument("--debug", action='store_true', help="Turn on debugging")
 
     args = parser.parse_args()
 
     curses.wrapper(main, args)
+# ·
+# ●
--- boids/boids/simulation.py
+++ boids/boids/simulation.py
@@ -10,7 +10,7 @@ from numpy.linalg import norm
 class Simulation:
     MIN_DISTANCE = 1.0
     MAX_RULE_VELO = 0.03
-    MAX_VELOCITY = .50
+    MAX_VELOCITY = .55
 
     COLOR_BOID = 1
     COLOR_BLACK_GREEN = 2
@@ -24,9 +24,11 @@ class Simulation:
         curses.init_pair(self.COLOR_DEBUG,
                          curses.COLOR_RED, curses.COLOR_BLACK)
 
-        self.__marker = kwargs.get("marker", ".")
+        self.__count = count
+        self.__marker = kwargs.get("marker", "·")
         self.__iteration_count = kwargs.get("iterations", 100)
-        self.__delay = kwargs.get("delay", 0.25)
+        self.__delay = kwargs.get("delay", 0.10)
+        self.__debug = kwargs.get("debug", False)
 
         self.__screen = stdscr
         self.__screen.nodelay(True)
@@ -34,32 +36,91 @@ class Simulation:
         self.__width = curses.COLS - 2
         self.__height = curses.LINES - 1
 
-        # position of each boid
-        # ...each being a random distance from the center up to 10 units away
-        # ...represented a `count` pairs of (x,y) coordinates
-        self.__boids = [self.__width / 2.0, self.__height / 2.0] + 5 * np.random.rand(2 * count).reshape(count, 2)
+        self.__seed_center()
+        # self.__seed_random()
 
         angles = 2 * math.pi * np.random.rand(count)
         self.vel = np.array(list(zip(np.cos(angles), np.sin(angles))))
-        self.__count = count
 
         self.__iteration = 1
 
-    def apply_boundary(self):
-        delta_r = 0.0
+    def __seed_center(self):
+        # position of each boid
+        # ...each being a random distance from the center up to 10 units away
+        # ...represented a `count` pairs of (x,y) coordinates
+        self.__boids = [self.__width / 2.0, self.__height / 2.0] + 5 * np.random.rand(2 * self.__count).reshape(self.__count, 2)
+
+    def __seed_random(self):
+        # self.__boids = [0.0, 0.0] + np.random.randint(43, size=2 * self.__count).reshape(self.__count, 2)
+        self.__boids = [self.__width, self.__height] * np.random.rand(2 * self.__count).reshape(self.__count, 2)
+
+    def apply_boundary_wrap(self):
+        delta_r = 0.99
 
         for coord in self.__boids:
+            # Check Col
             if coord[0] > self.__width + delta_r:
                 coord[0] = -delta_r
 
             if coord[0] < -delta_r:
                 coord[0] = self.__width + delta_r
 
+            # Check Row
             if coord[1] > self.__height + delta_r:
                 coord[1] = -delta_r
 
             if coord[1] < -delta_r:
                 coord[1] = self.__height + delta_r
+
+    def apply_boundary_reflect(self):
+        delta_r = 0.0
+        # reverse_flock = False
+        count = 0
+        bad_boids = []
+        for idx, coord in enumerate(self.__boids):
+            boid_ok = True
+
+            # Check Col
+            if coord[0] > self.__width:
+                coord[0] = self.__width - delta_r
+                reverse_flock = True
+                count += 1
+                # self.vel[idx][1] *= -1
+                boid_ok = False
+
+            if coord[0] < 0:
+                coord[0] = delta_r
+                # reverse_flock = True
+                count += 1
+                # self.vel[idx][1] *= -1
+                boid_ok = False
+
+            # Check Row
+            if coord[1] > self.__height:
+                coord[1] = self.__height - delta_r
+                # reverse_flock = True
+                count += 1
+                # self.vel[idx][0] *= -1
+                boid_ok = False
+
+            if coord[1] < 0:
+                coord[1] = delta_r
+                # reverse_flock = True
+                count += 1
+                # self.vel[idx][0] *= -1
+                boid_ok = False
+
+            if not boid_ok:
+                bad_boids.append(idx)
+
+        # If at least X hit boundary, reverse flock
+        if count > (self.__count * .25):
+            # for bidx in bad_boids:
+                # self.vel[bidx][0] *= -1
+                # self.vel[bidx][1] *= -1
+            self.vel *= -1
+            # self.vel[idx][0] *= -1
+            # self.vel[idx][1] *= -1
 
     def apply_rules(self):
         # get pair-wise distances
@@ -116,7 +177,7 @@ class Simulation:
         self.vel += self.apply_rules()
         self.limit(self.vel, self.MAX_VELOCITY)
         self.__boids += self.vel
-        self.apply_boundary()
+        self.apply_boundary_reflect()
 
         self.__boids.reshape(2*self.__count)[::2],
         self.__boids.reshape(2*self.__count)[1::2]
@@ -125,10 +186,22 @@ class Simulation:
 
     def display(self):
         self.__screen.erase()
-        for boid in self.__boids:
+
+        line = 0
+        for idx, boid in enumerate(self.__boids):
             col = int(boid[0])
             row = int(boid[1])
-            self.__screen.addstr(row, col, self.__marker, curses.color_pair(self.COLOR_BOID))
+            marker = idx if self.__debug else self.__marker
+            self.__screen.addstr(row, col, f"{marker}", curses.color_pair(self.COLOR_BOID))
+
+            if self.__debug:
+                self.__screen.addstr(line, 0, f"({row},{col})", curses.color_pair(self.COLOR_BOID))
+                line += 1
+
+        if self.__debug:
+            for vel in self.vel:
+                self.__screen.addstr(line, 0, f"{vel}", curses.color_pair(self.COLOR_DEBUG))
+                line += 1
 
         self.__screen.addstr(
             self.__height, 0,
