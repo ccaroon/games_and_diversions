8be3b28f600c90c5cfe03dee718f706a89a16b33
Craig N. Caroon <craig.caroon@cengage.com>
2024-04-08 14:43:29 -0400

    -- boids -- * decreased default delay * added some predefined markers * implemented add() method * cleaned up code a bit * boundary tweaks to keep from overreaching and erroring out * debuggin' & err handling

--- boids/boids.py
+++ boids/boids.py
@@ -26,12 +26,10 @@ if __name__ == "__main__":
     )
     parser.add_argument("--count", "-c", type=int, default=10, required=False, help="Number of Boids")
     parser.add_argument("--iterations", "-i", type=int, default=100, required=False, help="Number of iterations to run")
-    parser.add_argument("--delay", "-d", type=float, default=0.10, required=False, help="Delay between iterations")
+    parser.add_argument("--delay", "-d", type=float, default=0.075, required=False, help="Delay between iterations")
     parser.add_argument("--marker", "-m", type=str, default="·", required=False, help="Character to use as Boid marker")
     parser.add_argument("--debug", action='store_true', help="Turn on debugging")
 
     args = parser.parse_args()
 
     curses.wrapper(main, args)
-# ·
-# ●
