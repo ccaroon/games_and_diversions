0d66ba17bae369c0411312dea7c93794fb036ccb
Craig N. Caroon <craig.caroon@cengage.com>
2024-04-08 14:43:38 -0400

    -- boids -- * decreased default delay * added some predefined markers * implemented add() method * cleaned up code a bit * boundary tweaks to keep from overreaching and erroring out * debuggin' & err handling

--- boids/boids/simulation2.py
+++ boids/boids/simulation2.py
@@ -10,12 +10,18 @@ import time
 class Boid:
     MAX_HISTORY = 10
 
-    def __init__(self, x, y, dx, dy):
+    def __init__(self, x, y, dx=None, dy=None):
         self.x = x
         self.y = y
-        self.dx = dx
-        self.dy = dy
+        self.dx = dx or random.random() * 10 - 5
+        self.dy = dy or random.random() * 10 - 5
         self.__history = []
+
+    def __repr__(self):
+        return f"Boid({self.x},{self.y},{self.dx},{self.dy})"
+
+    def __str__(self):
+        return self.__repr__()
 
     def update_history(self):
         self.__history.append([self.x, self.y])
@@ -25,6 +31,12 @@ class Boid:
 
 class BoidSimulation2:
     VISUAL_RANGE = 75
+
+    MARKERS = {
+        "small-dot": "·",
+        "large-dot": "●"
+    }
+    DEFAULT_MARKER = MARKERS["small-dot"]
 
     COLOR_BOID = 1
     COLOR_BLACK_GREEN = 2
@@ -39,31 +51,38 @@ class BoidSimulation2:
                          curses.COLOR_RED, curses.COLOR_BLACK)
 
         self.__count = count
-        self.__marker = kwargs.get("marker", "·")
+        self.__marker = kwargs.get("marker", self.DEFAULT_MARKER)
+        if len(self.__marker) > 1:
+            self.__marker = self.MARKERS.get(self.__marker, self.DEFAULT_MARKER)
+
         self.__iteration_count = kwargs.get("iterations", 100)
-        self.__delay = kwargs.get("delay", 0.10)
+        self.__delay = kwargs.get("delay", 0.075)
         self.__debug = kwargs.get("debug", False)
 
         self.__screen = stdscr
         self.__screen.nodelay(True)
 
-        self.__width = curses.COLS - 2
+        self.__width = curses.COLS - 1
         self.__height = curses.LINES - 1
 
         self.__boids = []
-        self.__init_boids()
+        init_pattern = random.choice(("random", "center"))
+        self.__init_boids(pattern=init_pattern)
 
         self.__iteration = 1
 
-    def __init_boids(self):
+
+    def __init_boids(self, pattern="random"):
         for _ in range(self.__count):
+            if pattern == "random":
+                x = random.random() * self.__width
+                y = random.random() * self.__height
+            elif pattern == "center":
+                x = (self.__width / 2.0) + 5 * random.random()
+                y = (self.__height / 2.0) + 5 * random.random()
+
             self.__boids.append(
-                Boid(
-                    random.random() * self.__width,
-                    random.random() * self.__height,
-                    random.random() * 10 - 5,
-                    random.random() * 10 - 5,
-                )
+                Boid(x, y)
             )
 
 
@@ -73,27 +92,23 @@ class BoidSimulation2:
             (boid1.y - boid2.y) * (boid1.y - boid2.y)
         )
 
-    def __n_closest_boids(self, boid, n):
-        # Make a copy
-        # sorted = self.__boids.copy()
 
+    def __n_closest_boids(self, boid, n):
         cmp_func = lambda a,b: self.__distance(boid, a) - self.__distance(boid, b)
         key_func = functools.cmp_to_key(cmp_func)
+
         # Sort the copy by distance from `boid`
         sorted_boids = sorted(self.__boids, key=key_func)
-        # sorted.sort(
-        #     key=lambda a,b: self.__distance(boid, a) - self.__distance(boid, b)
-        # )
 
         # Return the `n` closest
         return sorted_boids[1:n + 1]
 
+
     # Constrain a boid to within the window. If it gets too close to an edge,
     # nudge it back in and reverse its direction.
     def __keep_within_bounds(self, boid):
-        # TODO: adjust margin
-        margin = 5
-        turn_factor = 3
+        margin = 3
+        turn_factor = 1
 
         if boid.x < margin:
             boid.dx += turn_factor
@@ -107,8 +122,9 @@ class BoidSimulation2:
         if boid.y > self.__height - margin:
             boid.dy -= turn_factor
 
-    # // Find the center of mass of the other boids and adjust velocity slightly to
-    # // point towards the center of mass.
+
+    # Find the center of mass of the other boids and adjust velocity slightly to
+    # point towards the center of mass.
     def __fly_towards_center(self, boid):
       # adjust velocity by this %
       centering_factor = 0.005
@@ -130,6 +146,7 @@ class BoidSimulation2:
         boid.dx += (center_x - boid.x) * centering_factor
         boid.dy += (center_y - boid.y) * centering_factor
 
+
     # Move away from other boids that are too close to avoid colliding
     def __avoid_others(self, boid):
       # The distance to stay away from other boids
@@ -149,6 +166,7 @@ class BoidSimulation2:
 
       boid.dx += move_x * avoid_factor
       boid.dy += move_y * avoid_factor
+
 
     # // Find the average velocity (speed and direction) of the other boids and
     # // adjust velocity slightly to match.
@@ -173,6 +191,7 @@ class BoidSimulation2:
         boid.dx += (avg_dx - boid.dx) * matching_factor
         boid.dy += (avg_dy - boid.dy) * matching_factor
 
+
     # Speed will naturally vary in flocking behavior, but real animals can't go
     # arbitrarily fast.
     def __limit_speed(self, boid):
@@ -184,42 +203,43 @@ class BoidSimulation2:
         boid.dy = (boid.dy / speed) * speed_limit
 
 
-    # TODO: implement
     def add(self, loc:tuple=None):
-        pass
-        # # Add a new Boid
-        # row = loc[0] or (self.__height // 2)
-        # col = loc[1] or (self.__width // 2)
-        # self.__boids = np.concatenate(
-        #     (self.__boids, np.array([[col,row]])),
-        #     axis=0
-        # )
-        # angles = 2 * math.pi * np.random.rand(1)
-        # v = np.array(list(zip(np.sin(angles), np.cos(angles))))
-        # self.vel = np.concatenate((self.vel, v), axis=0)
-        #
-        # self.__count += 1
+        # Add a new Boid
+        row = loc[0] if loc is not None else self.__height // 2
+        col = loc[1] if loc is not None else self.__width // 2
 
-    # TODO: implement
-    def scatter(self, loc:tuple=None):
-        pass
-        # row = loc[0] or random.randint(0, self.__height)
-        # col = loc[1] or random.randint(0, self.__width)
-        # self.vel += 0.1 * (self.__boids - np.array([[col, row]]))
+        self.__boids.append(Boid(col, row))
+        self.__count += 1
+
+
+    def __dump(self):
+        dump_str = ""
+        for boid in self.__boids:
+            if boid.x < 0 or boid.x > self.__width or boid.y < 0 or boid.y > self.__height:
+                dump_str += f"{boid}\n"
+        return dump_str
+
 
     def tick(self):
         for boid in self.__boids:
-            # // Update the velocities according to each rule
+            # Update the velocities according to each rule
             self.__fly_towards_center(boid)
             self.__avoid_others(boid)
             self.__match_velocity(boid)
             self.__limit_speed(boid)
             self.__keep_within_bounds(boid)
 
-            # // Update the position based on the current velocity
+            # Update the position based on the current velocity
             boid.x += boid.dx
+            # Keep in col/width bounds
+            boid.x = 0 if boid.x < 0 else boid.x
+            boid.x = self.__width if boid.x > self.__width else boid.x
+
             boid.y += boid.dy
-            boid.update_history()
+            # keep in row/height bounds
+            boid.y = 0 if boid.y < 0 else boid.y
+            boid.y = self.__height if boid.y > self.__height else boid.y
+            # boid.update_history()
 
         self.__iteration += 1
 
@@ -234,10 +254,10 @@ class BoidSimulation2:
             marker = idx if self.__debug else self.__marker
 
             if self.__debug:
-                self.__screen.addstr(line, 0, f"({row},{col}) -> [{angle}]/[{boid.dx},{boid.dy}]", curses.color_pair(self.COLOR_BOID))
+                self.__screen.addstr(line, 0, f"{idx}) ({row},{col}) -> [{angle}]/[{boid.dx},{boid.dy}]", curses.color_pair(self.COLOR_BOID))
                 line += 1
-            else:
-                self.__screen.addstr(row, col, f"{marker}", curses.color_pair(self.COLOR_BOID))
+
+            self.__screen.addstr(row, col, f"{marker}", curses.color_pair(self.COLOR_BOID))
 
         self.__screen.addstr(
             self.__height, 0,
@@ -250,21 +270,25 @@ class BoidSimulation2:
     def run(self):
         curses.mousemask(curses.ALL_MOUSE_EVENTS)
         for _ in range(self.__iteration_count):
-            self.display()
+
+            try:
+                self.display()
+            except curses.error as err:
+                xyzzy = self.__dump()
+                raise RuntimeError(xyzzy)
+
             self.tick()
 
             input = self.__screen.getch()
             if input == ord("q"):
                 break
-            elif input == ord("s"):
-                self.scatter()
             elif input == ord("b"):
                 self.add()
             elif input == curses.KEY_MOUSE:
                 id,x,y,_,button = curses.getmouse()
                 if button & curses.BUTTON1_CLICKED:
                     self.add((y,x))
-                if button & curses.BUTTON2_CLICKED:
-                    self.scatter((y,x))
+                # if button & curses.BUTTON2_CLICKED:
+                #     self.scatter((y,x))
 
             time.sleep(self.__delay)
